4855a53499fb78ac9b01822667618383b491b6fd
Pierre Spring <pierre.spring@caillou.ch>
2024-04-24 22:15:14 +0200

    learncpp - first exercise

---
+++ 01-hello-world/hello-world.cpp
@@ -0,0 +1,10 @@
+// https://www.learncpp.com/cpp-tutorial/compiling-your-first-program/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << std::endl
+              << "Hello, world!" << std::endl;
+    return 0;
+}
